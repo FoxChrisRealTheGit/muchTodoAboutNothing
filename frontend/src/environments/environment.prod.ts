0ad16db59f30944efad2a2f4524660c1071ba750
Stuff <stuffhoster_fda@protonmail.com>
2020-04-26 18:00:16 -0500

    a bit of facepalm

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const ENVIRONMENT = {
   production: true,
   environmentName: "Prod",
-  apiUrl: "ec2-18-216-70-175.us-east-2.compute.amazonaws.com/api",
-  baseURL: "ec2-18-216-70-175.us-east-2.compute.amazonaws.com",
+  apiUrl: "ec2-18-216-70-175.us-east-2.compute.amazonaws.com:80/api",
+  baseURL: "ec2-18-216-70-175.us-east-2.compute.amazonaws.com:80",
   clientVersion: "0.0.1"
 };
